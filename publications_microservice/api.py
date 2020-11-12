4daa0951a0ac7cd6e33e7d6497184892a85eac98
CrossNox <ijmermet@gmail.com>
2020-11-12 04:11:34 -0300

    allow for publications filtering

--- publications_microservice/api.py
+++ publications_microservice/api.py
@@ -1,12 +1,9 @@
 """API module."""
 from publications_microservice import __version__
-from flask_restx import Api, Resource, fields
+from flask_restx import Api, Resource, fields, reqparse
 from publications_microservice.models import Publication, db
-import logging
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
+import operator as ops
+from .utils import FilterParam
 
 api = Api(
     prefix="/v1",
@@ -56,17 +53,50 @@ publication_model = api.model(
 )
 
 
+publication_parser = reqparse.RequestParser()
+publication_parser.add_argument(
+    "bathrooms",
+    type=FilterParam("bathrooms", ops.ge),
+    help="minimum amount of bathrooms needed",
+    store_missing=False,
+)
+publication_parser.add_argument(
+    "rooms",
+    type=FilterParam("rooms", ops.ge),
+    help="minimum amount of rooms needed",
+    store_missing=False,
+)
+publication_parser.add_argument(
+    "beds",
+    type=FilterParam("beds", ops.ge),
+    help="minimum amount of beds needed",
+    store_missing=False,
+)
+publication_parser.add_argument(
+    "price_per_night",
+    type=FilterParam("price_per_night", ops.le),
+    help="max price per night",
+    store_missing=False,
+)
+
+
 @api.route('/publication')
 class PublicationListResource(Resource):
     @api.doc('list_publication')
     @api.marshal_list_with(publication_model)
+    @api.expect(publication_parser)
     def get(self):
         """Get all publications."""
-        return Publication.query.all()
+        params = publication_parser.parse_args()
+        api.app.logger.info("%s params", params)
+        query = Publication.query
+        for _, filter_op in params.items():
+            query = filter_op.apply(query, Publication)
+        return query.all()
 
     @api.doc('create_publication')
     @api.expect(publication_model)
-    @api.marshal_with(publication_model, envelope='resource')
+    @api.marshal_with(publication_model)
     def post(self):
         """Create a new publication."""
         new_publication = Publication(**api.payload)
@@ -80,7 +110,7 @@ class PublicationListResource(Resource):
 @api.response(404, 'Publication not found')
 class PublicationResource(Resource):
     @api.doc('get_publication')
-    @api.marshal_with(publication_model, envelope='resource')
+    @api.marshal_with(publication_model)
     def get(self, publication_id):
         """Get a publication by id."""
         publication = Publication.query.filter(Publication.id == publication_id).first()
--- publications_microservice/utils.py
+++ publications_microservice/utils.py
@@ -1,1 +1,23 @@
 """Helper functions for the project."""
+
+
+class FilterParam:
+    """Filter a query based on an op and param"""
+
+    def __init__(self, name, op):
+        self.name = name
+        self.op = op
+        self.val = None
+
+    def __call__(self, val):
+        self.val = val
+        return self
+
+    def apply(self, query, model):
+        return query.filter(self.op(getattr(model, self.name), self.val))
+
+    def __repr__(self):
+        return f"filter {self.name} by {self.op}"
+
+    def __str__(self):
+        return self.__repr__()
