114a1db09de3ed20f2a6421264b74018ce0bd512
CrossNox <ijmermet@gmail.com>
2020-12-19 14:50:59 -0300

    fix spatialite

--- publications_microservice/app.py
+++ publications_microservice/app.py
@@ -5,6 +5,10 @@ from pathlib import Path
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.event import listen
+from sqlalchemy.sql import func, select
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from publications_microservice.api import api
@@ -22,11 +26,20 @@ def fix_dialect(s):
     return s
 
 
+def load_spatialite(sqlite, _connection_rec):
+    logger.info("======== loading spatialite ========")
+    sqlite.enable_load_extension(True)
+    sqlite.load_extension('/usr/lib64/mod_spatialite.so')
+
+
 def create_app():
     """creates a new app instance"""
     new_app = Flask(__name__)
     new_app.config["SQLALCHEMY_DATABASE_URI"] = config.database.url(
-        default="sqlite:///publications_microservice.db", cast=fix_dialect
+        default="sqlite:///{curr_loc}/publications_microservice.db".format(
+            curr_loc=Path(__file__).parent
+        ),
+        cast=fix_dialect,
     )
     new_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(new_app)
@@ -37,4 +50,13 @@ def create_app():
     )  # remove after flask-restx > 0.2.0 is released
     # https://github.com/python-restx/flask-restx/issues/230
     CORS(new_app)
+
+    if new_app.config["SQLALCHEMY_DATABASE_URI"].startswith("sqlite"):
+        listen(Engine, 'connect', load_spatialite)
+        listen(Engine, 'first_connect', load_spatialite)
+        engine = create_engine(new_app.config["SQLALCHEMY_DATABASE_URI"], echo=True)
+        conn = engine.connect()
+        conn.execute(select([func.InitSpatialMetaData()]))
+        conn.close()
+
     return new_app
