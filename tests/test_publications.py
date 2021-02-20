d59b5f2d51f75fcb8e8bd826aa5d1350d8448850
CrossNox <ijmermet@gmail.com>
2021-02-20 20:08:19 -0300

    add test

--- tests/test_publications.py
+++ tests/test_publications.py
@@ -1,4 +1,5 @@
 """Sample test suite."""
+# pylint:disable=redefined-outer-name,protected-access
 import pytest
 
 from publications_microservice.app import create_app
@@ -10,7 +11,33 @@ def client():
         yield test_client
 
 
+@pytest.fixture
+def room_zero():
+    return {
+        "user_id": 0,
+        "title": "string",
+        "description": "string",
+        "rooms": 0,
+        "beds": 0,
+        "bathrooms": 0,
+        "price_per_night": 0,
+        "images": [{"url": "string"}],
+        "loc": {"latitude": 0, "longitude": 0},
+    }
+
+
 def test_root(client):  # pylint:disable=redefined-outer-name
     response = client.get("/")
     data = response.data
     assert data is not None
+    assert 200 == response._status_code
+
+
+def test_post(client, room_zero):  # pylint:disable=redefined-outer-name
+    response = client.post("/v1/publications", json=room_zero)
+    assert 200 == response._status_code
+
+
+def test_post_wo_prefix(client, room_zero):  # pylint:disable=redefined-outer-name
+    response = client.post("/publications", json=room_zero)
+    assert 404 == response._status_code
