45c5d388a95179927c71431e87c41b2d24df4b10
CrossNox <ijmermet@gmail.com>
2021-02-21 02:04:56 -0300

    fxs

--- tests/test_publications.py
+++ tests/test_publications.py
@@ -1,27 +1,33 @@
 """Sample test suite."""
+import json
+import logging
+
 # pylint:disable=redefined-outer-name,protected-access
 import pytest
 
 from publications_microservice.app import create_app
 
+logger = logging.getLogger(__name__)
+
 
 @pytest.fixture
 def client():
-    with create_app().test_client() as test_client:
+    app = create_app()
+    with app.test_client() as test_client:
         yield test_client
 
 
 @pytest.fixture
 def room_zero():
     return {
-        "user_id": 0,
-        "title": "string",
-        "description": "string",
-        "rooms": 0,
-        "beds": 0,
-        "bathrooms": 0,
-        "price_per_night": 0,
-        "images": [{"url": "string"}],
+        "user_id": 1,
+        "title": "Publication title",
+        "description": "Some nice description",
+        "rooms": 1,
+        "beds": 1,
+        "bathrooms": 1,
+        "price_per_night": 10,
+        "images": [],
         "loc": {"latitude": 0, "longitude": 0},
     }
 
@@ -36,6 +42,7 @@ def test_root(client):
 def test_post(client, room_zero):
     response = client.post("/v1/publications", json=room_zero)
     assert 200 == response._status_code
+    assert len(json.loads(response.data)) >= 1
 
 
 def test_post_wo_prefix(client, room_zero):
