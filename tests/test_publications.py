be0e9d63425df4e91a0652f6d94d2f144d6f5ad7
CrossNox <ijmermet@gmail.com>
2021-03-03 06:52:23 -0300

    Show blocked (#53)
    * bump
    
    * fias
    
    * fixes

--- tests/test_publications.py
+++ tests/test_publications.py
@@ -47,7 +47,7 @@ def test_root(client):
 def test_post(client, room_zero):
     response = client.post("/v1/publications", json=room_zero)
     assert 200 == response._status_code
-    assert len(json.loads(response.data)) >= 1
+    assert json.loads(response.data) is not None
 
 
 def test_loc_search(client, room_zero):
@@ -55,7 +55,12 @@ def test_loc_search(client, room_zero):
     assert response._status_code == 200
     response = client.get(
         "/v1/publications",
-        json={"latitude": 0.1, "longitude": 0.1, "max_distance": 15.7},
+        json={
+            "blockchain_status": "UNSET",
+            "latitude": 0.1,
+            "longitude": 0.1,
+            "max_distance": 15.7,
+        },
     )
     assert response._status_code == 200
     assert len(json.loads(response.data)) == 1
@@ -64,3 +69,157 @@ def test_loc_search(client, room_zero):
 def test_post_wo_prefix(client, room_zero):
     response = client.post("/publications", json=room_zero)
     assert 404 == response._status_code
+
+
+def test_filter_blocked_default_blockchain_default(client, room_zero):
+    # UNSET and not blocked
+    r1 = client.post("/v1/publications", json=room_zero)
+    assert r1._status_code == 200
+
+    # UNSET and blocked
+    r2 = client.post("/v1/publications", json=room_zero)
+    assert r2._status_code == 200
+    r = client.delete(f"/v1/publications/{json.loads(r2.data).get('id')}")
+    assert r._status_code == 200
+
+    # CONFIRMED and not blocked
+    r3 = client.post("/v1/publications", json=room_zero)
+    assert r3._status_code == 200
+    r = client.patch(
+        f"/v1/publications/{json.loads(r3.data).get('id')}",
+        json={"blockchain_status": "CONFIRMED"},
+    )
+    assert r._status_code == 200
+
+    # CONFIRMED and blocked
+    r4 = client.post("/v1/publications", json=room_zero)
+    assert r4._status_code == 200
+    r = client.patch(
+        f"/v1/publications/{json.loads(r4.data).get('id')}",
+        json={"blockchain_status": "CONFIRMED"},
+    )
+    assert r._status_code == 200
+    r = client.delete(f"/v1/publications/{json.loads(r4.data).get('id')}")
+    assert r._status_code == 200
+
+    search = client.get("/v1/publications")
+    assert search._status_code == 200
+    search_data = json.loads(search.data)
+    assert len(search_data) == 1
+    assert search_data[0]["id"] == json.loads(r3.data)["id"]
+
+
+def test_filter_blocked_default_blockchain_explicit(client, room_zero):
+    # UNSET and not blocked
+    r1 = client.post("/v1/publications", json=room_zero)
+    assert r1._status_code == 200
+
+    # UNSET and blocked
+    r2 = client.post("/v1/publications", json=room_zero)
+    assert r2._status_code == 200
+    r = client.delete(f"/v1/publications/{json.loads(r2.data).get('id')}")
+    assert r._status_code == 200
+
+    # CONFIRMED and not blocked
+    r3 = client.post("/v1/publications", json=room_zero)
+    assert r3._status_code == 200
+    r = client.patch(
+        f"/v1/publications/{json.loads(r3.data).get('id')}",
+        json={"blockchain_status": "CONFIRMED"},
+    )
+    assert r._status_code == 200
+
+    # CONFIRMED and blocked
+    r4 = client.post("/v1/publications", json=room_zero)
+    assert r4._status_code == 200
+    r = client.patch(
+        f"/v1/publications/{json.loads(r4.data).get('id')}",
+        json={"blockchain_status": "CONFIRMED"},
+    )
+    assert r._status_code == 200
+    r = client.delete(f"/v1/publications/{json.loads(r4.data).get('id')}")
+    assert r._status_code == 200
+
+    search = client.get("/v1/publications", data={"blockchain_status": "CONFIRMED"})
+    assert search._status_code == 200
+    search_data = json.loads(search.data)
+    assert len(search_data) == 1
+    assert search_data[0]["id"] == json.loads(r3.data)["id"]
+
+
+def test_filter_blocked_true_blockchain_implicit(client, room_zero):
+    # UNSET and not blocked
+    r1 = client.post("/v1/publications", json=room_zero)
+    assert r1._status_code == 200
+
+    # UNSET and blocked
+    r2 = client.post("/v1/publications", json=room_zero)
+    assert r2._status_code == 200
+    r = client.delete(f"/v1/publications/{json.loads(r2.data).get('id')}")
+    assert r._status_code == 200
+
+    # CONFIRMED and not blocked
+    r3 = client.post("/v1/publications", json=room_zero)
+    assert r3._status_code == 200
+    r = client.patch(
+        f"/v1/publications/{json.loads(r3.data).get('id')}",
+        json={"blockchain_status": "CONFIRMED"},
+    )
+    assert r._status_code == 200
+
+    # CONFIRMED and blocked
+    r4 = client.post("/v1/publications", json=room_zero)
+    assert r4._status_code == 200
+    r = client.patch(
+        f"/v1/publications/{json.loads(r4.data).get('id')}",
+        json={"blockchain_status": "CONFIRMED"},
+    )
+    assert r._status_code == 200
+    r = client.delete(f"/v1/publications/{json.loads(r4.data).get('id')}")
+    assert r._status_code == 200
+
+    search = client.get("/v1/publications", data={"filter_blocked": "true"})
+    assert search._status_code == 200
+    search_data = json.loads(search.data)
+    assert len(search_data) == 1
+    assert search_data[0]["id"] == json.loads(r3.data)["id"]
+
+
+def test_filter_blocked_false_blockchain_implicit(client, room_zero):
+    # UNSET and not blocked
+    r1 = client.post("/v1/publications", json=room_zero)
+    assert r1._status_code == 200
+
+    # UNSET and blocked
+    r2 = client.post("/v1/publications", json=room_zero)
+    assert r2._status_code == 200
+    r = client.delete(f"/v1/publications/{json.loads(r2.data).get('id')}")
+    assert r._status_code == 200
+
+    # CONFIRMED and not blocked
+    r3 = client.post("/v1/publications", json=room_zero)
+    assert r3._status_code == 200
+    r = client.patch(
+        f"/v1/publications/{json.loads(r3.data).get('id')}",
+        json={"blockchain_status": "CONFIRMED"},
+    )
+    assert r._status_code == 200
+
+    # CONFIRMED and blocked
+    r4 = client.post("/v1/publications", json=room_zero)
+    assert r4._status_code == 200
+    r = client.patch(
+        f"/v1/publications/{json.loads(r4.data).get('id')}",
+        json={"blockchain_status": "CONFIRMED"},
+    )
+    assert r._status_code == 200
+    r = client.delete(f"/v1/publications/{json.loads(r4.data).get('id')}")
+    assert r._status_code == 200
+
+    search = client.get("/v1/publications", data={"filter_blocked": "false"})
+    assert search._status_code == 200
+    search_data = json.loads(search.data)
+    assert len(search_data) == 2
+    assert set([search_data[0]["id"], search_data[1]["id"]]) == set(
+        [json.loads(r3.data)["id"], json.loads(r4.data)["id"]]
+    )
